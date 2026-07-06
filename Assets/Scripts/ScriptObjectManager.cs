2f6ea2c4c58fbae6082e42704f6f850d590aebe8
redCaramel <parkjunesung0912@gmail.com>
2026-07-06 09:38:58 +0900

    [Fix] Add Script List Initial Spacing

--- Assets/Scripts/ScriptObjectManager.cs
+++ Assets/Scripts/ScriptObjectManager.cs
@@ -63,7 +63,7 @@ public class ScriptObjectManager : MonoBehaviour
             rect.anchorMin = new Vector2(0.5f, 1f);
             rect.anchorMax = new Vector2(0.5f, 1f);
             rect.pivot = new Vector2(0.5f, 1f);
-            rect.anchoredPosition = new Vector2(0f, -i * (itemHeight + scriptSpacing));
+            rect.anchoredPosition = new Vector2(0f, -i * (itemHeight + scriptSpacing) - scriptSpacing);
 
             slot.GetComponent<Button>().onClick.AddListener(() => OnScriptClicked(i));
             Scripts.Add(slot);

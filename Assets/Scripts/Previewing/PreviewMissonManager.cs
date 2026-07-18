89b724f95cf7e20f83efeec65ae3d640ce4601a9
redCaramel <parkjunesung0912@gmail.com>
2026-07-19 03:26:17 +0900

    [Feat] Implement Clear/Fail Popup

---
+++ Assets/Scripts/Previewing/PreviewMissonManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class PreviewMissonManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("targetObject"))
+        {
+            Debug.Log("asdf");
+            PreviewMissonResultManager.instance.MissonSuccess();
+        }
+    }
+}

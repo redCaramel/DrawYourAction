83ef8e782be012f48dea98ee36ad3a874ce48c6b
redCaramel <parkjunesung0912@gmail.com>
2026-07-22 02:53:42 +0900

    [Fix] Replace old Action if new Action Loaded

--- Assets/Scripts/Action/ActionExecuter.cs
+++ Assets/Scripts/Action/ActionExecuter.cs
@@ -54,6 +54,12 @@ public class ActionExecuter : MonoBehaviour
 
     public void StartLoading(List<Action> acts)
     {
+        // 이전 카드의 행동이 아직 실행 중이었다면, 새 카드를 적용하기 전에 즉시 취소
+        if (loading)
+        {
+            PlayerController.instance.StopMovement();
+        }
+
         _queue = new Queue<Action>(acts);
         _elapsedTime = 0f;
         loading = true;
